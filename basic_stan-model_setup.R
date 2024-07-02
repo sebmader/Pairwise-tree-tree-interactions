4c23d060cc38aae9a42d1692e249bb45fa8e9a38
Mader <sebastianmader@gmx.net>
2024-07-02 16:49:30 +0200

    add basic stan model script

---
+++ basic_stan-model_setup.R
@@ -0,0 +1,73 @@
+### Model test ###
+
+library(rstan)
+library(coda)
+library(BayesianTools)
+
+test.pairwise.trees <- "
+  data{
+    int ND; // number of total data points
+    int NS; // number of species
+    real focal_bm[ND];  // biomass of focal tree
+    real next_bm[ND];   // the next year biomass of corresponding focal tree
+    int fsp_ID[ND]; // identify species of each data point
+  }
+  
+  parameters{
+    real<lower=0> beta[NS]; // species specific growth parameter
+    real theta;             // test if the growth rate scale with power law (0.75) 
+    real<lower=0> sigma;    // sigma is observation error 
+  }
+  
+  model{
+    // for model simplification
+    real predBM;
+    
+    // specify priors
+    for (i in 1:NS){
+      beta[i] ~ exponential(1.0); 	  
+  	}
+    theta ~ normal(0.75, 1); // based on 3/4 exponent
+    sigma ~ normal(0,1);
+     
+    for(i in 1:ND){    // loop over all data points
+          // prediction
+          predBM = focal_bm[i] + beta[fsp_ID[i]]*focal_bm[i]^theta; 
+  		
+          // likelihood		
+         next_bm[i] ~ lognormal( log(predBM), sigma );
+    }
+  }
+  
+  generated quantities {
+    real predBM; 
+    vector[ND] log_lik;  // number of log likelihood for loo
+    
+    for (i in 1:ND){    // loop through focal individual tree
+        // prediction
+        predBM = focal_bm[i] + beta[fsp_ID[i]]*focal_bm[i]^theta; 
+  	  // pointwise log likelihood
+  	  log_lik[i] = lognormal_lpdf( next_bm[i] | log(predBM), sigma);
+    }
+  }
+"
+
+inits <- rep(list(list(sigma = 1.0)), 3)
+
+# compile model and sampling
+mod = stan_model(model_code = test.pairwise.trees)
+stan_fit <- sampling(  mod,
+                       data = data.1,
+                       iter = 1000,
+                       warmup = 500,
+                       thin = 1,
+                       chains = 3, 
+                       init = inits,
+                       control = list(max_treedepth = 10,
+                                      adapt_delta = 0.9))
+
+print(stan_fit, pars = c("beta", "theta", "sigma"), digits = 3, probs = c(0.025, 0.975))
+
+# save the output stan_fit object
+# saveRDS(stan_fit, file = res)
+
